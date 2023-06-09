f94097514f451eeb80f5b6062320b2386add98be
xuwei <xuwei@thvr.com>
2023-06-09 12:18:15 +0800

    publish.sh

--- .publish.sh
+++ .publish.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-git fetch --tags
+git checkout master
+mv .git/temp/* ./
+git add .
+git commit -a -m "build binary"
 latest=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo -e "Latest tag is "$latest
 read -p "Please input version(ex: v1.0) for publising: " version

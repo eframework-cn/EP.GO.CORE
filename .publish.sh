5085c02222253f1dc99af01e2ca450c337648274
xuwei <xuwei@thvr.com>
2023-06-09 15:36:07 +0800

    .publish.sh

--- .publish.sh
+++ .publish.sh
@@ -14,6 +14,6 @@ latest=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo -e "Latest tag is "$latest
 read -p "Please input version(ex: v1.0.0) for publising: " version
 git tag ${version}
-git push
+git push --tags
 git checkout encrypt
 read -p "publish done."

e2467bf4d2483a572cb33739720e6830c2544af0
xuwei <xuwei@thvr.com>
2023-06-09 14:27:27 +0800

    .publish.sh

--- .publish.sh
+++ .publish.sh
@@ -9,4 +9,5 @@ echo -e "Latest tag is "$latest
 read -p "Please input version(ex: v1.0) for publising: " version
 git tag ${version}
 git push origin ${version}
+git checkout encrypt
 read -p "publish done."

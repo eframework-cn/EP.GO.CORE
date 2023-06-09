2e5a9af1973ddb002c1949fc4b433fc733fee69d
xuwei <xuwei@thvr.com>
2023-06-09 14:53:16 +0800

    .publish.sh

--- .publish.sh
+++ .publish.sh
@@ -6,8 +6,8 @@ git add .
 git commit -a -m "build binary"
 latest=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo -e "Latest tag is "$latest
-read -p "Please input version(ex: v1.0) for publising: " version
+read -p "Please input version(ex: v1.0.0) for publising: " version
 git tag ${version}
-git push origin ${version}
+git push
 git checkout encrypt
 read -p "publish done."

5920c86215e1000b0e26e0d38a5d33c99e736110
wellshsu <wellshsu@outlook.com>
2023-07-11 16:12:04 +0800

    update publish

--- .publish.sh
+++ .publish.sh
@@ -15,7 +15,7 @@ latest=$(git describe --tags `git rev-list --tags --max-count=1`)
 echo -e "Latest tag is "$latest
 read -p "Please input version(ex: v1.0.0) for publising: " version
 git tag ${version}
-git push --tags
 git push
+git push --tags
 git checkout encrypt
 read -p "publish done."
